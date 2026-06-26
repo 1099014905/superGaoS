854f2226914bbbee039ac96cd828acbd05faf93c
admin <admin@supergaos.com>
2026-06-26 21:37:09 +0800

    feat: create supergaos-seckill module skeleton

---
+++ supergaos-seckill/src/main/java/com/supergaos/seckill/common/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.supergaos.seckill.common;
+
+import com.supergaos.common.exception.BusinessException;
+import com.supergaos.common.result.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public Result<Void> handleBusinessException(BusinessException e) {
+        return Result.error(e.getErrorCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public Result<Void> handleException(Exception e) {
+        return Result.error(500, "服务器内部错误: " + e.getMessage());
+    }
+}
